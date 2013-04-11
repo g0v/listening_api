e71db96a00bd5b423ef406445e65ac688e46474e
wildjcrt <wildjcrt@yahoo.com.tw>
2013-04-11 20:08:40 +0800

    create breadcrumbs. application_helper add safe_truncate_content

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_admin_user!
+
+  def breadcrumbs
+    @breadcrumbs = []
+    @breadcrumbs << ['後台', root_path]
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,33 @@
 module ApplicationHelper
+  def render_ul_breadcrumbs
+    @breadcrumbs.delete []
+
+    content_tag :ul, :class => 'breadcrumb' do
+      last_breadcrumb_name = @breadcrumbs.pop.first
+      @breadcrumbs.map do |breadcrumb|
+        content_tag :li do
+          if breadcrumb.first.is_a? Array
+            breadcrumb.map{|e| link_with_breadcrumb(e)}.join(', ').html_safe
+          else
+            link_with_breadcrumb(breadcrumb)
+          end
+        end
+      end.push(content_tag :li, last_breadcrumb_name, :class => 'active')
+         .join('<li><span class="divider"><i class="icon-chevron-right"></i></span></li>')
+         .html_safe
+    end
+  end
+
+  def link_with_breadcrumb(link_info)
+    link_name = link_info.first
+    path, params = link_info.last.split('?')
+    params = params.nil? ? [] : params.split('&')
+    params << 'ref=breadcrumb'
+    link_to(link_name, "#{path}?#{params.join('&')}")
+  end
+
+  def safe_truncate_content(content, length)
+    pure_text = strip_tags(content).gsub(/\n/, '').gsub(/\r/, '').gsub(/\t/, '').gsub(/&nbsp;/, '')
+    sanitize truncate(pure_text, :length => length, :omission => '…')
+  end
 end
